42469d58dedf065ace68794d1d44e58be443b85e
Steve Rector <steverector27@gmail.com>
2024-02-08 11:12:28 -0800

    Displays platform icons for each game

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Game } from "../hooks/useGames";
 import { Card, CardBody } from "@chakra-ui/card";
 import { Image } from "@chakra-ui/image";
-import { Heading } from "@chakra-ui/layout";
+import { Heading, Text } from "@chakra-ui/layout";
+import PlatformIconList from "./PlatformIconList";
 
 interface GameCardProps {
   game: Game;
@@ -14,6 +15,9 @@ const GameCard = ({ game }: GameCardProps) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
