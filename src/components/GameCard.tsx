373c7f5e8c84a93aac16e2c9c7eb4154ab9d1f35
Steve Rector <steverector27@gmail.com>
2024-02-08 10:42:46 -0800

    Add gamecards instead of just the name when games list is shown

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody } from "@chakra-ui/card";
+import { Image } from "@chakra-ui/image";
+import { Heading } from "@chakra-ui/layout";
+
+interface GameCardProps {
+  game: Game;
+}
+
+const GameCard = ({ game }: GameCardProps) => {
+  return (
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
