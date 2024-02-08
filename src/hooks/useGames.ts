19efe4427bb356f04ea4e6946cd8fddf57f9a864
Steve Rector <steverector27@gmail.com>
2024-02-08 13:30:28 -0800

    Add skeleton cards during loading for styling

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -26,6 +26,7 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const URL = "https://api.rawg.io/api";
 
@@ -34,16 +35,20 @@ const useGames = () => {
 
     apiClient
       .get<FetchGamesResponse>(`${URL}/games`, { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setIsLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.mesage);
+        setIsLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
